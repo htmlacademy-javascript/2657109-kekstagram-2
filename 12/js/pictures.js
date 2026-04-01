bf3f08c2ebaa56d2f209d62dd71f635de4db28d1
Keks <keks@htmlacademy.ru>
2026-04-01 16:06:25 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/pictures.js
@@ -0,0 +1,39 @@
+import { openBigPicture } from './big-picture.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const pictureElements = () => picturesContainer.querySelectorAll('.picture');
+
+function renderPictures(photos) {
+  pictureElements().forEach((pictureElement) => pictureElement.remove());
+
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const commentsCount = photo.comments.length;
+    const pictureElement = pictureTemplate.cloneNode(true);
+    const pictureImageElement = pictureElement.querySelector('.picture__img');
+    const pictureLikesElement = pictureElement.querySelector('.picture__likes');
+    const pictureCommentsElement = pictureElement.querySelector('.picture__comments');
+
+    pictureImageElement.src = photo.url;
+    pictureImageElement.alt = photo.description;
+    pictureLikesElement.textContent = photo.likes;
+    pictureCommentsElement.textContent = commentsCount;
+
+    // Добавлено: открываем полноразмерный просмотр по клику на миниатюру.
+    const onPictureClick = (evt) => {
+      evt.preventDefault();
+      openBigPicture(photo);
+    };
+    pictureElement.addEventListener('click', onPictureClick);
+
+    fragment.appendChild(pictureElement);
+  });
+
+  picturesContainer.appendChild(fragment);
+}
+
+export { renderPictures };
